fbce4cfa8f0d62d1a91798813836522124143b2e
Tunahan Dogan <piyealar@gmail.com>
2023-05-07 10:03:55 +0300

    desc

--- pages/_app.js
+++ pages/_app.js
@@ -14,6 +14,30 @@ function MyApp({ Component, pageProps }) {
           href="https://upload.wikimedia.org/wikipedia/commons/a/a6/Vodafone_icon.svg"
         />
         <title>Kolay Paket Yükle - Vodafone</title>
+
+        {/* Açıklama meta etiketi */}
+        <meta
+          name="description"
+          content="Kolay Paket Yükle - Vodafone ile paket yüklemelerinizi hızlı ve kolayca gerçekleştirebilirsiniz."
+        />
+
+        {/* Open Graph etiketleri (Facebook, Instagram, WhatsApp vb.) */}
+        <meta property="og:title" content="Kolay Paket Yükle - Vodafone" />
+        <meta
+          property="og:description"
+          content="Kolay Paket Yükle - Vodafone ile paket yüklemelerinizi hızlı ve kolayca gerçekleştirebilirsiniz."
+        />
+        <meta property="og:url" content="https://vodafone-paket.vercel.app/" />
+        <meta property="og:type" content="website" />
+
+        {/* Twitter Card etiketleri */}
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:title" content="Kolay Paket Yükle - Vodafone" />
+        <meta
+          name="twitter:description"
+          content="Kolay Paket Yükle - Vodafone ile paket yüklemelerinizi hızlı ve kolayca gerçekleştirebilirsiniz."
+        />
+        <meta name="twitter:url" content="https://vodafone-paket.vercel.app/" />
       </Head>
       <Navbar />
       <Component {...pageProps} />
